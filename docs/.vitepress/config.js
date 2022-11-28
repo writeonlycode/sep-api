ada02845dba8b064a37051f3f0f71279d8e9ec3b
Iago Bozza <iagobozza@writeonlycode.io>
2022-11-28 12:22:34 +0530

    Update github pages configuration

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 export default {
   title: "SEP API",
   description: "An unofficial API for the Stanford Encyclopedia of Philosophy.",
+  base: "/sep-api/",
   themeConfig: {
     sidebar: [
       {
