da5ff062b6aa88d067f39d4929aebc52f19ca76f
Iago Bozza <iagobozza@writeonlycode.io>
2023-01-03 04:11:49 +0000

    Update favicons

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,13 @@ export default {
   base: "/sep-api/",
   cleanUrls: "with-subfolders",
 
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+  ],
+
   themeConfig: {
     nav: [{ text: "API Reference", link: "/reference/what-is-the-sep-api" }],
     sidebar: [
